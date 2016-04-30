af8eb820437cc2c07c54fcef24aa651eaeb78183
Marcus Floriano <marcus.floriano@gmail.com>
2016-04-30 20:20:23 -0300

    create de struct for UI

--- src/majs.js
+++ src/majs.js
@@ -9,4 +9,12 @@ MAJS.init = function(params) {
   if(!document.getElementById(MAJS.container)){
     throw "the container for MAJS not defined";
   }
+  MAJS.draw = SVG(MAJS.container).size(350,250).spof();
+
+  MAJS.main = MAJS.draw.rect(250, 250);
+  MAJS.main.style("fill:#f2f2f2");
+
+  MAJS.menu = MAJS.draw.rect(100, 250);
+  MAJS.menu.move(250,0);
+  MAJS.menu.style("fill:#e6e6e6");
 };
