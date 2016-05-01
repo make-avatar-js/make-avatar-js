bc4fe6aa700c5f590b209ab0cb49f82841c7ec19
Marcus Floriano <marcus.floriano@gmail.com>
2016-05-01 19:07:42 -0300

    alter the struct base

--- src/majs.js
+++ src/majs.js
@@ -3,18 +3,29 @@
 
 var MAJS = MAJS || {};
 
+MAJS.scene = function() {
+  this.main = MAJS._draw.rect(250, 250);
+  this.menu = MAJS._draw.rect(100, 250);
+};
+
+MAJS.scene.prototype.prepare = function() {
+  this.menu.move(250,0);
+  this.menu.style("fill:rgb(230, 230, 230)");
+  this.main.style("fill:rgb(242, 242, 242)");
+};
+
 MAJS.init = function(params) {
+
   MAJS.name = "Make Avatar JS";
+
   MAJS.container = (params["container"] == undefined)? "majs" : params["container"];
   if(!document.getElementById(MAJS.container)){
     throw "the container for MAJS not defined";
   }
-  MAJS.draw = SVG(MAJS.container).size(350,250).spof();
 
-  MAJS.main = MAJS.draw.rect(250, 250);
-  MAJS.main.style("fill:#f2f2f2");
+  MAJS._draw = SVG(MAJS.container).size(350,250).spof();
 
-  MAJS.menu = MAJS.draw.rect(100, 250);
-  MAJS.menu.move(250,0);
-  MAJS.menu.style("fill:#e6e6e6");
+  MAJS._scene = new MAJS.scene();
+  MAJS._scene.prepare();
+
 };
