e6e7e9d5fde9f66ed764b2ed3af13bb66aa5d1ed
Marcus Floriano <marcus.floriano@gmail.com>
2016-05-01 19:38:37 -0300

    the adjustable size of scene

--- src/majs.js
+++ src/majs.js
@@ -4,12 +4,12 @@
 var MAJS = MAJS || {};
 
 MAJS.scene = function() {
-  this.main = MAJS._draw.rect(250, 250);
-  this.menu = MAJS._draw.rect(100, 250);
+  this.main = MAJS._draw.rect(MAJS._width * 0.75, MAJS._height);
+  this.menu = MAJS._draw.rect(MAJS._width * 0.25, MAJS._height);
 };
 
 MAJS.scene.prototype.prepare = function() {
-  this.menu.move(250,0);
+  this.menu.move(MAJS._width * 0.75,0);
   this.menu.style("fill:rgb(230, 230, 230)");
   this.main.style("fill:rgb(242, 242, 242)");
 };
@@ -23,7 +23,10 @@ MAJS.init = function(params) {
     throw "the container for MAJS not defined";
   }
 
-  MAJS._draw = SVG(MAJS.container).size(350,250).spof();
+  MAJS._width = (params["width"] == undefined)? 350 : params["width"];
+  MAJS._height = (params["height"] == undefined)? 250 : params["height"];
+
+  MAJS._draw = SVG(MAJS.container).size(MAJS._width,MAJS._height).spof();
 
   MAJS._scene = new MAJS.scene();
   MAJS._scene.prepare();
